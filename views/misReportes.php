768e36eab4b00258c92ffbfccf5d96c4cc29ca6e
BalamPerales <balam.perales465@gmail.com>
2026-03-17 09:59:36 -0600

    Modal de reportes.

--- views/misReportes.php
+++ views/misReportes.php
@@ -8,13 +8,25 @@
   <div class="card border-0 shadow my-5">
     <div class="card-body p-5">
       <h1 class="fw-light">Reportes</h1>
-      <p class="lead">Content on the page</p>
+      <p class="lead">
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalCrearReporte">
+          Crear Reporte
+        </button>
+        <hr>
+        <div id="tablaReporteClienteLoad"></div>
+      </p>
     </div>
   </div>
 </div>
 
 <?php 
+  include "reportesCliente/modalCrearReporte.php";
   include "footer.php" ;
+?>
+
+<script src="../public/js/reportesCliente/reportesCliente.js"></script>
+
+<?php
   } else {
     header("location:../index.html");
   }
