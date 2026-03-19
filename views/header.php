0a8711d199812a747f5664edbd86566fcfe8e2f1
BalamPerales <balam.perales465@gmail.com>
2026-03-19 04:10:27 -0600

    Implementación del nuevo diseño.

--- views/header.php
+++ views/header.php
@@ -1,102 +1,101 @@
-<?php 
-  session_start(); 
+<?php
+  session_start();
 
   if (!isset($_SESSION['usuario'])) {
-        header("Location: ../index.html"); 
-        exit();
-    }
-
+    header("Location: ../index.html");
+    exit();
+  }
 ?>
- 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../public/bootstrap/bootstrap.min.css" />
-    <link rel="stylesheet" href="../public/css/plantilla.css" />
-    <link rel="stylesheet" href="../public/datatable/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="../public/datatable/responsive.bootstrap4.min.css">
-    <link rel="stylesheet" href="../public/fontawesome/css/all.css">
-    <link rel="stylesheet" href="../public/datatable/buttons.dataTables.min.css">
-    <title>Help Desk</title>
-  </head>
-  <body>
-    <!-- Navigation -->
-    <nav
-      class="navbar navbar-expand-lg navbar-light bg-light static-top mb-5 shadow"
-    >
-      <div class="container">
-        <a class="navbar-brand" href="inicio.php">
-          <img src="../public/img/logoicono.ico" alt="Icono" width="30%">
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarResponsive"
-          aria-controls="navbarResponsive"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="inicio.php">
-                <span class="fas fa-home"></span> Inicio
-              </a>
-            </li>
-            <?php if ($_SESSION['usuario']['rol'] == 3) { ?>
-            <li class="nav-item">
-              <a class="nav-link" href="misDispositivos.php">
-                <span class="fas fa-server"></span> Mis dispositivos
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="misReportes.php">
-                <spans class="fas fa-file-alt"></spans> Reportes soporte
-              </a>
-            </li>
-            <?php } else if($_SESSION['usuario']['rol'] == 1){  ?>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="../public/bootstrap/bootstrap.min.css" />
+  <link rel="stylesheet" href="../public/datatable/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="../public/datatable/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="../public/fontawesome/css/all.css">
+  <link rel="stylesheet" href="../public/datatable/buttons.dataTables.min.css">
+  
+  <link rel="stylesheet" href="../public/css/triforce_logo.css">
+  <link rel="stylesheet" href="../public/css/zelda_style.css"> 
+  <title>Help Desk</title>
+  <link rel="icon" href="../public/img/logoicono.ico" type="image/x-icon">
+</head>
+<body class="dashboard-page"> <nav class="navbar navbar-expand-lg navbar-dark bg-zelda-dark static-top mb-5 shadow"> <div class="container">
+     <a class="navbar-brand d-flex align-items-center" href="inicio.php">
+        <img src="../public/img/logoicono.ico" alt="Logo" style="width: 30px; height: 30px;" class="me-2">
+        Help Desk
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarResponsive"
+        aria-controls="navbarResponsive"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="inicio.php">
+              <span class="fas fa-home"></span> Inicio
+            </a>
+          </li>
 
-            <!-- Las vistas del administrador -->
-            <li class="nav-item">
-              <a class="nav-link" href="usuarios.php">
-                <span class="fas fa-user-cog"></span> Usuarios
+          <?php if ($_SESSION['usuario']['rol'] == 3) { ?>
+          <li class="nav-item">
+            <a class="nav-link" href="misDispositivos.php">
+              <span class="fas fa-server"></span> Mis dispositivos
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="misReportes.php">
+              <span class="fas fa-file-alt"></span> Reportes soporte
+            </a>
+          </li>
+          <?php } else if($_SESSION['usuario']['rol'] == 1) { ?>
+
+          <li class="nav-item">
+            <a class="nav-link" href="usuarios.php">
+              <span class="fas fa-user-cog"></span> Usuarios
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="asignacion.php">
+              <span class="fas fa-address-book"></span> Asignación
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="reportes.php">
+              <span class="fas fa-file-alt"></span> Reportes
+            </a>
+          </li>
+          <?php } ?>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: var(--zelda-gold);">
+              <span class="fas fa-user"></span> Usuario: <?php echo $_SESSION['usuario']['nombre']; ?>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right shadow-lg" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalActualizarDatosPersonales" onclick="obtenerDatosPersonalesInicio('<?php echo $_SESSION['usuario']['id_usuario']; ?>')">
+                <span class="fas fa-edit"></span> Editar datos
               </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="asignacion.php">
-                <span class="fas fa-address-book"></span> Asignación
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item text-danger" href="../procesos/usuarios/login/salir.php">
+                <span class="fas fa-sign-out-alt"></span> Salir del Sistema
               </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="reportes.php">
-                <spans class="fas fa-file-alt"></spans> Reportes
-              </a>
-            </li>
-            <?php } ?>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle"  style="color:blue" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="far fa-user"></span> Usuario: <?php echo $_SESSION['usuario']['nombre']; ?>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalActualizarDatosPersonales" onclick="obtenerDatosPersonalesInicio('<?php echo $_SESSION['usuario']['id'];?>')">
-                  <span class="far fa-edit"></span> Editar datos
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item"  style="color:red" href="../procesos/usuarios/login/salir.php">
-                  <span class="fas fa-sign-out-alt"></span> Salir
-                </a>
-              </div>
-            </li>
-          </ul>
-        </div>
+            </div>
+          </li>
+
+        </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
 
-    <?php
-      include "inicio/modalActualizarDatosPersonales.php";
-    ?>
+  <?php
+    include "inicio/modalActualizarDatosPersonales.php";
+  ?>
